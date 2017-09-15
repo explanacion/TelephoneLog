6e78f6c516ce6232fa17c1d6e153c44a64d2c88c
explanacion <odexed@mail.ru>
2017-09-15 13:47:44 +0700

    Первая рабочая версия. Просмотр лога телефонных звонков, сортировка столбцов, подсветка удачных и неудачных входящих и исходящих

---
+++ TelephoneLog/Form1.Designer.cs
@@ -0,0 +1,359 @@
+﻿namespace TelephoneLog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown5 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown6 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Event = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Details = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(6, 44);
+            this.monthCalendar1.MaxSelectionCount = 1;
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(6, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Выбрать день";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(6, 248);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            24,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(48, 20);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(60, 248);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown2.TabIndex = 3;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(112, 248);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown3.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(6, 219);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "От";
+            // 
+            // numericUpDown4
+            // 
+            this.numericUpDown4.Location = new System.Drawing.Point(6, 309);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
+            24,
+            0,
+            0,
+            0});
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(48, 20);
+            this.numericUpDown4.TabIndex = 2;
+            this.numericUpDown4.Value = new decimal(new int[] {
+            23,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown5
+            // 
+            this.numericUpDown5.Location = new System.Drawing.Point(60, 309);
+            this.numericUpDown5.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.numericUpDown5.Name = "numericUpDown5";
+            this.numericUpDown5.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown5.TabIndex = 3;
+            this.numericUpDown5.Value = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown6
+            // 
+            this.numericUpDown6.Location = new System.Drawing.Point(112, 309);
+            this.numericUpDown6.Maximum = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            this.numericUpDown6.Name = "numericUpDown6";
+            this.numericUpDown6.Size = new System.Drawing.Size(46, 20);
+            this.numericUpDown6.TabIndex = 3;
+            this.numericUpDown6.Value = new decimal(new int[] {
+            59,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(6, 280);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 24);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "До";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.date,
+            this.Event,
+            this.Details,
+            this.Column1});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.dataGridView1.Location = new System.Drawing.Point(199, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(532, 615);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // date
+            // 
+            this.date.Frozen = true;
+            this.date.HeaderText = "Дата";
+            this.date.Name = "date";
+            this.date.ReadOnly = true;
+            this.date.Width = 80;
+            // 
+            // Event
+            // 
+            this.Event.Frozen = true;
+            this.Event.HeaderText = "Событие";
+            this.Event.Name = "Event";
+            this.Event.ReadOnly = true;
+            this.Event.Width = 76;
+            // 
+            // Details
+            // 
+            this.Details.Frozen = true;
+            this.Details.HeaderText = "Подробности";
+            this.Details.Name = "Details";
+            this.Details.ReadOnly = true;
+            this.Details.Width = 99;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Длительность";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(6, 416);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 34);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Путь";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 390);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(157, 20);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(6, 363);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 24);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Путь к логам";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(6, 522);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(176, 34);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Найти";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 486);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(157, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(6, 459);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(184, 24);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Шаблон имени лога";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(731, 615);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown6);
+            this.Controls.Add(this.numericUpDown5);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.monthCalendar1);
+            this.Name = "Form1";
+            this.Text = "Telephone Log v1.0 (Неклюдов Константин <odexed@mail.ru>) для ППЦ. 2017г.";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown4;
+        private System.Windows.Forms.NumericUpDown numericUpDown5;
+        private System.Windows.Forms.NumericUpDown numericUpDown6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn date;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Event;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Details;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+    }
+}
+
---
+++ TelephoneLog/Form1.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Globalization;
+
+namespace TelephoneLog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = Properties.Settings.Default["logpath"].ToString();
+            textBox2.Text = Properties.Settings.Default["filepattern"].ToString();
+            int rwidth = (dataGridView1.Width - dataGridView1.Columns[0].Width) / 3;
+            dataGridView1.Columns[1].Width = rwidth;
+            dataGridView1.Columns[2].Width = rwidth;
+            dataGridView1.Columns[3].Width = rwidth;
+            //dataGridView1.Rows.Add(new string[] { "column2 value", "column6 value", "test" });
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                // resize controls
+                dataGridView1.Size = new Size(Form1.ActiveForm.Width - 200, 0);
+                int rwidth = (dataGridView1.Width - dataGridView1.Columns[0].Width) / 3;
+                dataGridView1.Columns[1].Width = rwidth;
+                dataGridView1.Columns[2].Width = rwidth;
+                dataGridView1.Columns[3].Width = rwidth;
+            }
+            catch (Exception)
+            { 
+            }
+        }
+
+        private void ReadFromLogFile(string filename)
+        {
+            try
+            {
+                bool success = false;
+                using (var fs = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                using (var sr = new StreamReader(fs, Encoding.Default))
+                {
+                    string[] lines = sr.ReadToEnd().Split('\n');
+                    
+                    DateTime talkingtill = DateTime.Now;
+                    DateTime talkingsince = DateTime.Now;
+                    string cdate = monthCalendar1.SelectionRange.Start.ToString("dd-MM-yy");
+                    foreach (string line in lines)
+                    {
+                        if (line.IndexOf("Line DialNum_New") != -1)
+                        {
+                            // outcome call
+                            string since = numericUpDown1.Value.ToString("00") + ":" + numericUpDown2.Value.ToString("00") + ":" + numericUpDown3.Value.ToString("00");
+                            DateTime dateTimeSince = DateTime.ParseExact(cdate + " " + since, "dd-MM-yy HH:mm:ss", CultureInfo.InvariantCulture);
+                            string till = numericUpDown4.Value.ToString("00") + ":" + numericUpDown5.Value.ToString("00") + ":" + numericUpDown6.Value.ToString("00");
+                            DateTime dateTimeTill = DateTime.ParseExact(cdate + " " + till, "dd-MM-yy HH:mm:ss", CultureInfo.InvariantCulture);
+
+                            string[] linearr = line.Split(new char[] { ' ' });
+                            string curtime = linearr[0];
+                            DateTime curdatetime = DateTime.ParseExact(cdate + " " + curtime, "dd-MM-yy HH:mm:ss", CultureInfo.InvariantCulture);
+
+                            // trying to get a duration
+                            DateTime tempsince, temptill;
+                            tempsince = talkingsince;
+                            temptill = talkingtill;
+                            string duration = "";
+                            try
+                            {
+                                //Console.WriteLine(talkingsince.ToString("dd-MM-yy HH:mm:ss"));
+                                TimeSpan durspan = (temptill - tempsince);
+                                duration = durspan.ToString(@"hh\:mm\:ss");
+                            }
+                            catch (Exception)
+                            {
+                            }
+
+                            if (curdatetime > dateTimeSince && curdatetime < dateTimeTill)
+                            {
+                                string substr = linearr.Last();
+                                substr = substr.Substring(1, substr.Length - 3);
+                                
+                                if (tempsince.ToString("dd-MM-yy HH:mm:ss") != "")
+                                    dataGridView1.Rows.Add(new string[] { curtime, "Исходящий звонок", substr, duration });
+                            }
+                        }
+                        else if (line.IndexOf("Get CallerId=") != -1)
+                        {
+                            // income call
+                            string since = numericUpDown1.Value.ToString("00") + ":" + numericUpDown2.Value.ToString("00") + ":" + numericUpDown3.Value.ToString("00");
+                            DateTime dateTimeSince = DateTime.ParseExact(cdate + " " + since, "dd-MM-yy HH:mm:ss", CultureInfo.InvariantCulture);
+                            string till = numericUpDown4.Value.ToString("00") + ":" + numericUpDown5.Value.ToString("00") + ":" + numericUpDown6.Value.ToString("00");
+                            DateTime dateTimeTill = DateTime.ParseExact(cdate + " " + till, "dd-MM-yy HH:mm:ss", CultureInfo.InvariantCulture);
+                            string[] linearr = line.Split(new char[] { ' ' });
+                            string curtime = linearr[0];
+                            DateTime curdatetime = DateTime.ParseExact(cdate + " " + curtime, "dd-MM-yy HH:mm:ss", CultureInfo.InvariantCulture);
+
+                            // trying to get a duration
+                            DateTime tempsince, temptill;
+                            tempsince = talkingsince;
+                            temptill = talkingtill;
+                            string duration = "";
+                            try
+                            {
+                                //Console.WriteLine(talkingsince.ToString("dd-MM-yy HH:mm:ss"));
+                                TimeSpan durspan = (temptill - tempsince);
+                                duration = durspan.ToString(@"hh\:mm\:ss");
+                            }
+                            catch (Exception ex)
+                            {
+                            }
+
+                            if (curdatetime > dateTimeSince && curdatetime < dateTimeTill)
+                            {
+                                string substr = linearr.Last();
+                                substr = substr.Replace("CallerId=", "");
+                                substr = substr.Substring(1, substr.Length - 2);
+                                dataGridView1.Rows.Add(new string[] { curtime, "Входящий звонок", substr, duration });
+
+                            }
+                        }
+                        else if (line.IndexOf("==14:Talking") != -1)
+                        {
+                            // start talking time
+                            string[] linearr = line.Split(new char[] { ' ' });
+                            string talksince = linearr[0];
+                            talkingsince = DateTime.ParseExact(cdate + " " + talksince, "dd-MM-yy HH:mm:ss", CultureInfo.InvariantCulture);
+                        }
+                        else if (line.IndexOf("Fxs Hangup") != -1)
+                        {
+                            // stop talking time
+                            string[] linearr = line.Split(new char[] { ' ' });
+                            string talktill = linearr[0];
+                            talkingtill = DateTime.ParseExact(cdate + " " + talktill, "dd-MM-yy HH:mm:ss", CultureInfo.InvariantCulture);
+                        }
+                        else if (line.IndexOf("=ACCEPT") != -1)
+                        {
+                            success = true;
+                            int lrow = dataGridView1.Rows.GetLastRow(DataGridViewElementStates.Displayed);
+                            if (success)
+                                dataGridView1.Rows[lrow - 1].Cells[0].Style.BackColor = Color.LightGreen;
+                        }
+                        else if (line.IndexOf("=CONNECTED") != -1)
+                        {
+                            success = true;
+                            int lrow = dataGridView1.Rows.GetLastRow(DataGridViewElementStates.Displayed);
+                            if (success)
+                                dataGridView1.Rows[lrow - 1].Cells[0].Style.BackColor = Color.LightGreen;
+                        }
+                        else if (line.IndexOf("=BUSY") != -1)
+                        {
+                            success = false;
+                            int lrow = dataGridView1.Rows.GetLastRow(DataGridViewElementStates.Displayed);
+                            if (!success)
+                                dataGridView1.Rows[lrow - 1].Cells[0].Style.BackColor = Color.LightPink;
+                        }
+
+                    }
+                }
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                MessageBox.Show("Файл лога, соответствующий дате не найден");
+            }
+            catch (DirectoryNotFoundException)
+            {
+                MessageBox.Show("Директория не найдена");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            string dir = textBox1.Text;
+            if (!Directory.Exists(dir))
+            {
+                MessageBox.Show("Внимание. Директория " + dir + " не найдена.");
+            }
+            string[] filenames = textBox2.Text.Split(new char[] {'|'});
+            //string sMonth = DateTime.Now.ToString("MM");
+            //string sDay = DateTime.Now.ToString("dd");
+            string sMonth = monthCalendar1.SelectionRange.Start.ToString("MM");
+            string sDay = monthCalendar1.SelectionRange.Start.ToString("dd");
+            for (int i = 0; i < filenames.Length; i++)
+            {
+                string filepath = dir + "\\" + filenames[i] + "." + sMonth + sDay;
+                ReadFromLogFile(filepath);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+        }
+    }
+}
